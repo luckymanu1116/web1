abb36c3772c745775ad3fa836dac193afd93f5ce
Jaswanth <Jaswanth@DESKTOP-87A37JV>
2022-07-21 23:09:08 -0400

    Add project files.

---
+++ web1/Pages/Intro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web1.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
